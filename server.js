6a2e9c7ae0c5feb41cb234f8c85814825ef886b9
lynnvdbo <lynn.van.den.bosch@hva.nl>
2026-02-18 00:40:07 +0100

    route /vibe-emoji aangemaakt #8

--- server.js
+++ server.js
@@ -171,3 +171,21 @@ app.get('/oud-jong', async function (request, response) {
   response.render('verjaardag.liquid', {persons: personResponseJSON.data})
 })
 
+
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!! Dit linked naar mijn VIBE-EMOJI pagina !!!!!!!!!!!!!!!!!!!!!!!!!!!!
+app.get('/vibe-emoji', async function (request, response) {
+  const params = {
+    'sort': 'name',
+    'fields': '*,squads.*',
+    // Combineer meerdere filters
+    'filter[squads][squad_id][tribe][name]': 'FDND Jaar 1',
+    // Filter eventueel alleen op een bepaalde squad
+    // 'filter[squads][squad_id][name]': '1I',
+    // 'filter[squads][squad_id][name]': '1J',
+    'filter[squads][squad_id][cohort]': '2526'
+  }
+  const personResponse = await fetch('https://fdnd.directus.app/items/person/?' + new URLSearchParams(params))
+  const personResponseJSON = await personResponse.json()
+  response.render('vibe-emoji.liquid', {persons: personResponseJSON.data})
+})
+
